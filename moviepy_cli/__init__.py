6e2337bd0217bcc058b6b4019b773fe6726f114b
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-07-07 15:24:12 +0900

    Add --end option

--- moviepy_cli/__init__.py
+++ moviepy_cli/__init__.py
@@ -10,10 +10,13 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument("input_file", type=path.Path, help="input file")
-    parser.add_argument("--start", type=float, help="start time")
+    parser.add_argument("--start", help="start time")
+    parser.add_argument("--end", help="end time")
     parser.add_argument("--retime", type=float, help="retime")
-    parser.add_argument("--inplace", action="store_true", help="inplace")
-    parser.add_argument("--output-file", type=path.Path, help="output file")
+    parser.add_argument("--inplace", "-i", action="store_true", help="inplace")
+    parser.add_argument(
+        "--output-file", "-o", type=path.Path, help="output file"
+    )
     args = parser.parse_args()
 
     args.input_file = args.input_file.abspath()
@@ -21,7 +24,7 @@ def main():
     clip = moviepy.editor.VideoFileClip(args.input_file)
 
     if args.start:
-        clip = clip.subclip(args.start)
+        clip = clip.subclip(args.start, args.end)
     if args.retime:
         clip = clip.fx(moviepy.editor.vfx.speedx, args.retime)
 
